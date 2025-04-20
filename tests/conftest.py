50ee5947a78e8f56a0090dbe0478ef163d6cdf61
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-20 19:10:40 +0300

    Добавление conftest

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from selene import browser, Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True,
+            "screenResolution": "1920x1080x24"
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = 'https://demoqa.com'
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
